b84d5b7b563b945322882374375657c2cf269de8
THOMAS-PC\thoma <thomasystenes@gmail.com>
2022-11-25 14:59:08 +0100

    Edited to use default profile pic path instead of web link

--- project/lib/utils/user_settings.dart
+++ project/lib/utils/user_settings.dart
@@ -2,7 +2,7 @@ import '../model/user.dart';
 
 class UserSettings {
   static User currentUser = User(
-      "https://images.unsplash.com/photo-1554151228-14d9def656e4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=333&q=80",
+      "images\\defaultProfilePic.jpg",
       "Jane Doe",
       "123 Fakestreet",
       "eu12345",
@@ -11,3 +11,6 @@ class UserSettings {
       [],
       "user");
 }
+
+//old profile pic from web
+//"https://images.unsplash.com/photo-1554151228-14d9def656e4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=333&q=80"
