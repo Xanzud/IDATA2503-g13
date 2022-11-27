b6faadcd6ea5dc4631e479b020902cb5d685b3af
Streafe <streafe@gmail.com>
2022-11-27 16:49:59 +0100

    Update firestore_repository.dart

--- project/lib/services/firestore_repository.dart
+++ project/lib/services/firestore_repository.dart
@@ -8,17 +8,16 @@ import '../../services/repository.dart';
 import 'api_paths.dart';
 
 class FirestoreRepository implements Repository {
-
   //TODO Temporarily disabled i guess, use getAllMissionsStreamWithID() instead
-/*
+
   @override
   Stream<Iterable<Mission>> getAllMissionsStream() => FirebaseCrud()
-      .getCollectionStream(ApiPaths.missionRoot(), Mission.fromMap);
+      .getCollectionStream(ApiPaths.missionRoot(), Mission.fromMapOriginal);
 
   @override
   Stream<Mission?> getMissionStream(String missionId) => FirebaseCrud()
-      .getDocumentStream(ApiPaths.mission(missionId), Mission.fromMap);
-*/
+      .getDocumentStream(ApiPaths.mission(missionId), Mission.fromMapOriginal);
+
   @override
   Future<void> createMission(Mission mission, String missionID) =>
       FirebaseCrud().setData(
@@ -28,26 +27,14 @@ class FirestoreRepository implements Repository {
 
   @override
   Stream<Iterable<User>> getUsersStream() {
-    return FirebaseCrud().getCollectionStream(
-        ApiPaths.userRoot(), User.fromMap);
+    return FirebaseCrud()
+        .getCollectionStream(ApiPaths.userRoot(), User.fromMap);
   }
 
   /// Retrieves all missions together with the ID
   @override
-  Stream<Iterable<Mission>> getAllMissionsStreamWithID() => FirebaseCrud().collectionStreamWithID(
-      builder: (data, documentID) => Mission.fromMapFactory(data, documentID),
-  );
-
-  //TODO placeholders, too tired to remove atm.
-  @override
-  Stream<Iterable<Mission>> getAllMissionsStream() {
-    // TODO: implement getAllMissionsStream
-    throw UnimplementedError();
-  }
-
-  @override
-  Stream<Mission?> getMissionStream(String missionId) {
-    // TODO: implement getMissionStream
-    throw UnimplementedError();
-  }
+  Stream<Iterable<Mission>> getAllMissionsStreamWithID() =>
+      FirebaseCrud().collectionStreamWithID(
+        builder: (data, documentID) => Mission.fromMapFactory(data, documentID),
+      );
 }
