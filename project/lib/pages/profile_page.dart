01c23a98bf6f1d45807000b5900a5e68b2bb25cc
Streafe <streafe@gmail.com>
2022-11-28 14:23:17 +0100

    fixed some weird bug with double 'User' import

--- project/lib/pages/profile_page.dart
+++ project/lib/pages/profile_page.dart
@@ -6,7 +6,6 @@ import "../utils/user_settings.dart";
 import "../widget/profile_widget.dart";
 import "../model/user.dart";
 
-
 class ProfilePage extends StatefulWidget {
   @override
   _ProfilePageState createState() => _ProfilePageState();
@@ -19,14 +18,12 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: _buildAppBar(context),
-      body: ListView(
-        physics: const BouncingScrollPhysics(),
-        children: [
+        appBar: _buildAppBar(context),
+        body: ListView(physics: const BouncingScrollPhysics(), children: [
           const SizedBox(height: 24),
           ProfileWidget(
-              imagePath: _user.imagePath,
-              onClicked: () async {},
+            imagePath: _user.imagePath,
+            onClicked: () async {},
           ),
           const SizedBox(height: 24),
           buildNameAndEmail(_user),
@@ -37,28 +34,27 @@ class _ProfilePageState extends State<ProfilePage> {
           const SizedBox(height: 24),
           buildInfoField(infoName: "Reg #", info: _user.regNr),
           const SizedBox(height: 24),
-          buildInfoField(infoName: "Certifications",
-              info: _user.certifications.toString()
-          ),
+          buildInfoField(
+              infoName: "Certifications",
+              info: _user.certifications.toString()),
           const SizedBox(height: 24),
-          ElevatedButton(onPressed:() {onEditButtonPressed();},
-              child: Text(  _isEditButtonPressed ? "Save" : "Edit",
+          ElevatedButton(
+              onPressed: () {
+                onEditButtonPressed();
+              },
+              child: Text(
+                _isEditButtonPressed ? "Save" : "Edit",
                 style: TextStyle(color: Colors.black),
-              )
-          ),
+              )),
           const SizedBox(height: 24),
           ElevatedButton(
-          onPressed: (() => onLoadUserButtonPressed()), 
-          child: const Text("Load user")
-          ),
+              onPressed: (() => onLoadUserButtonPressed()),
+              child: const Text("Load user")),
           const SizedBox(height: 24),
           ElevatedButton(
-          onPressed: (() => print(_user.name)), 
-          child: const Text("Print current user name")
-          ),
-        ]
-      )
-    );
+              onPressed: (() => print(_user.name)),
+              child: const Text("Print current user name")),
+        ]));
   }
 
   AppBar _buildAppBar(BuildContext context) {
@@ -66,18 +62,12 @@ class _ProfilePageState extends State<ProfilePage> {
     return AppBar(
       leading: const BackButton(),
       elevation: 0,
-      actions: [
-        IconButton(
-            onPressed: () {},
-            icon: const Icon(icon))
-      ],
+      actions: [IconButton(onPressed: () {}, icon: const Icon(icon))],
     );
   }
 
-
   /// Builds fields for name and email address.
-  Widget buildNameAndEmail(User user) => Column(
-      children: [
+  Widget buildNameAndEmail(User user) => Column(children: [
         Text(
           user.name,
           style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
@@ -87,40 +77,34 @@ class _ProfilePageState extends State<ProfilePage> {
           user.email,
           style: const TextStyle(color: Colors.grey),
         )
-      ]
-  );
-
+      ]);
 
   /// Builds generic info field. Intended for easy creation of Address, phone,
   /// and reg.
-  Widget buildInfoField({required String infoName, required String info}) => Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
+  Widget buildInfoField({required String infoName, required String info}) =>
+      Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
         Padding(
             padding: const EdgeInsets.only(left: 10),
             child: Text(
               infoName,
               style: const TextStyle(fontWeight: FontWeight.w500, fontSize: 20),
-            )
-        ),
+            )),
         Padding(
             padding: const EdgeInsets.only(left: 10, right: 10),
             child: TextField(
               enabled: _isEditButtonPressed,
-            )
-        )
-      ]
-  );
+            ))
+      ]);
 
   ///Method to be called when edit button is pressed
   ///Simply toggles isEditButtonPressed.
-  void onEditButtonPressed(){
+  void onEditButtonPressed() {
     setState(() {
       _isEditButtonPressed = !_isEditButtonPressed;
     });
   }
 
-  void onLoadUserButtonPressed(){
+  void onLoadUserButtonPressed() {
     UserHandler.loadUser();
     print("onLoadUserButtonPresse User: ${_user.name}");
     setState(() {
