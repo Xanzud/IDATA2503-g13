592642499fe34fbfb19199faa160f5dfae8984a5
Streafe <streafe@gmail.com>
2022-12-02 21:19:36 +0100

    added attending list to missions

--- project/lib/model/Mission.dart
+++ project/lib/model/Mission.dart
@@ -5,8 +5,9 @@ class Mission {
   final Timestamp time;
   final String location;
   final String id;
+  final List<String> attending;
 
-  Mission(this.name, this.time, this.location, this.id);
+  Mission(this.name, this.time, this.location, this.id, this.attending);
 
   //TODO Cleanup, don't need 2 fromMaps
   static Mission fromMap(Map<String, dynamic> data, String id) {
@@ -14,7 +15,8 @@ class Mission {
     assert(data.containsKey("time"), "Missing time property for a mission");
     assert(data.containsKey("location"),
         "Missing location property for a mission");
-    return Mission(data["name"], data["time"], data["location"], data["id"]);
+    return Mission(data["name"], data["time"], data["location"], data["id"],
+        data["attending"]);
   }
 
   static Mission fromMapOriginal(Map<String, dynamic> data) {
@@ -23,15 +25,17 @@ class Mission {
     assert(data.containsKey("location"),
         "Missing location property for a mission");
     assert(data.containsKey("id"), "Missing id property for a mission");
-    return Mission(data["name"], data["time"], data["location"], data["id"]);
+    return Mission(data["name"], data["time"], data["location"], data["id"],
+        data["attending"]);
   }
 
   factory Mission.fromMapFactory(Map<String, dynamic> data, String documentId) {
     final String name = data["name"];
     final Timestamp time = data["time"];
     final String location = data["location"];
+    final List<String> attending = data["attending"];
 
-    return Mission(name, time, location, documentId);
+    return Mission(name, time, location, documentId, attending);
   }
 
   Map<String, dynamic> toMap() {
@@ -40,6 +44,7 @@ class Mission {
       "name": name,
       "time": time,
       "location": location,
+      "attending": attending
     };
   }
 }
