d51958bca78edefe0705dabe11be329bbc497706
Amfrost <amfrost@stud.ntnu.no>
2022-12-04 20:38:49 +0100

    Cleaned up some code

--- project/lib/services/firestore_service.dart
+++ project/lib/services/firestore_service.dart
@@ -21,38 +21,4 @@ class FirestoreService {
     print('delete: $path');
     await reference.delete();
   }
-
-/*
-  Stream<List<T>> collectionStream<T>({
-    required String path,
-    required T Function(Map<String, dynamic> data, String documentId) builder,
-    Query Function(Query query)? queryBuilder,
-    int Function(T lhs, T rhs)? sort,
-  }) {
-    Query query = FirebaseFirestore.instance.collection(path);
-    if (queryBuilder != null) {
-      query = queryBuilder(query);
-    }
-    final snapshots = query.snapshots();
-    return snapshots.map((snapshot) {
-      final result = snapshot.docs
-          .map((snapshot) => builder(snapshot.data(), snapshot.id))
-          .where((value) => value != null)
-          .toList();
-      if (sort != null) {
-        result.sort(sort);
-      }
-      return result;
-    });
-  }
-  */
-
-  Stream<T> documentStream<T>({
-    required String path,
-    required T builder(Map<String, dynamic> data, String documentID),
-  }) {
-    final reference = FirebaseFirestore.instance.doc(path);
-    final snapshots = reference.snapshots();
-    return snapshots.map((snapshot) => builder(snapshot.data()!, snapshot.id));
-  }
 }
--- project/test/login_page_test.dart
+++ project/test/login_page_test.dart
@@ -1,5 +1,4 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:project/landingPage.dart';
 import 'package:project/signInPage.dart';
 
 void main(){
