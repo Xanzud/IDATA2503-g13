f1f82ea19f2c637123cd8d4ede900aa502cc4396
Amfrost <amfrost@stud.ntnu.no>
2022-12-04 18:42:07 +0100

    Cleaned up the feed page code a bit

--- project/lib/pages/feed_page_admin.dart
+++ project/lib/pages/feed_page_admin.dart
@@ -1,8 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:project/missions/mission_page.dart';
-import 'package:project/missions/new_mission_page.dart';
 import 'package:project/model/Mission.dart';
 import 'package:project/pages/admin_page.dart';
 import 'package:project/pages/packing_list_page.dart';
@@ -10,10 +8,8 @@ import 'package:project/pages/profile_page.dart';
 import 'package:project/services/firestore_repository.dart';
 import 'package:project/services/repository.dart';
 import 'package:provider/provider.dart';
-
 import '../missions/edit_mission_page.dart';
 import '../utils/DateFormatter.dart';
-import '../utils/user_settings.dart';
 
 class LandingPageAdmin extends StatefulWidget {
   @override
@@ -48,7 +44,6 @@ class _LandingPageAdminState extends State<LandingPageAdmin> {
 
   @override
   Widget build(BuildContext context) {
-    final user = UserSettings.currentUser;
     return Provider<Repository>(
         create: (context) => FirestoreRepository(),
         builder: (context, child) {
@@ -103,114 +98,13 @@ class _LandingPageAdminState extends State<LandingPageAdmin> {
     );
   }
 
-  Widget _buildMissionInfo(BuildContext context) {
-    const String missionId = "g1tXCcqtk1YSV45o9p6v";
-    final repository = Provider.of<Repository>(context, listen: false);
-    bool isChecked = false;
-
-    return StreamBuilder<Mission?>(
-        stream: repository.getMissionStream(missionId),
-        builder: (context, snapshot) {
-          // Check if we got something other than real data...
-          if (snapshot.connectionState != ConnectionState.active) {
-            return Center(
-              child: const CircularProgressIndicator.adaptive(),
-            );
-          } else if (snapshot.hasError) {
-            return Center(
-              child: Text("Error: ${snapshot.error}"),
-            );
-          } else if (!snapshot.hasData || snapshot.data == null) {
-            return const Center(
-              child: Text("No data to load"),
-            );
-          }
-
-          // If we get so far, this means we got data!
-          final Mission mission = snapshot.data!;
-
-          return Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Container(
-                    height: 150,
-                    decoration: BoxDecoration(
-                        color: Colors.grey[300],
-                        borderRadius: BorderRadius.circular(20),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey,
-                            blurRadius: 1,
-                            offset: Offset(1, 4),
-                          )
-                        ]),
-                    padding: const EdgeInsets.all(10.0),
-                    child: Column(
-                      children: <Widget>[
-                        Align(
-                            alignment: Alignment.topLeft,
-                            child: Padding(
-                              padding: EdgeInsets.all(5),
-                              child: Text(mission.name,
-                                  style: TextStyle(color: Colors.red[300])),
-                            )),
-                        Align(
-                            alignment: Alignment.centerLeft,
-                            child: Padding(
-                              padding: EdgeInsets.all(10),
-                              child: Text(mission.name,
-                                  style: TextStyle(
-                                      color: Colors.blue[700], fontSize: 18)),
-                            )),
-                        Align(
-                          alignment: Alignment.bottomLeft,
-                          child: Padding(
-                              padding: EdgeInsets.fromLTRB(10, 20, 0, 0),
-                              child: Row(
-                                children: [
-                                  Wrap(
-                                    crossAxisAlignment:
-                                        WrapCrossAlignment.center,
-                                    spacing: 5,
-                                    children: [
-                                      Icon(Icons.access_time_outlined),
-                                      Text(mission.time.toString()),
-                                    ],
-                                  ),
-                                  Spacer(),
-                                  Wrap(
-                                    crossAxisAlignment:
-                                        WrapCrossAlignment.center,
-                                    spacing: 5,
-                                    children: [
-                                      Text('Attend',
-                                          style: TextStyle(fontSize: 16)),
-                                      Checkbox(
-                                        checkColor: Colors.white,
-                                        value: isChecked,
-                                        onChanged: (bool? value) {
-                                          setState(() => isChecked = value!);
-                                        },
-                                      )
-                                    ],
-                                  )
-                                ],
-                              )),
-                        ),
-                      ],
-                    )),
-              ]);
-        });
-  }
-
   Widget _buildMissionInfoAll(BuildContext context) {
     final repository = Provider.of<Repository>(context, listen: false);
-    bool isChecked = false;
 
     return StreamBuilder<Iterable<Mission>>(
         stream: repository.getAllMissionsStreamWithID(),
         builder: (context, snapshot) {
-          // Check if we got something other than real data...
+          // Error handling
           if (snapshot.connectionState != ConnectionState.active) {
             return Center(
               child: const CircularProgressIndicator.adaptive(),
@@ -225,7 +119,6 @@ class _LandingPageAdminState extends State<LandingPageAdmin> {
             );
           }
 
-          // If we get so far, this means we got data!
           final Iterable<Mission> mission = snapshot.data!;
 
           return Column(
@@ -337,37 +230,5 @@ class _LandingPageAdminState extends State<LandingPageAdmin> {
             ),
           ],
         ));
-  }
-
-  Widget _buildMissionTitle(String missionName) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 8),
-      child: Text(
-        missionName,
-        style: const TextStyle(fontSize: 24),
-      ),
-    );
-  }
-
-  /// Build the price text for the product
-  Widget _buildMissionLocation(String location) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 8, bottom: 8),
-      child: Text(
-        "location: $location",
-        style: const TextStyle(fontSize: 16),
-      ),
-    );
-  }
-
-  /// Build the product image
-  Widget _buildMissionTime(String time) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 8, bottom: 8),
-      child: Text(
-        "time: $time",
-        style: const TextStyle(fontSize: 16),
-      ),
-    );
   }
 }
