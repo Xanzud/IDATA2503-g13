7933bd103c1b4c393be1e40ecc931dea9bec181e
THOMAS-PC\thoma <thomasystenes@gmail.com>
2022-12-05 01:05:26 +0100

    Made simple unit test for PackingItem.toMap()

---
+++ project/test/packing_item_test.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:project/firebase_options.dart';
+import 'package:project/main.dart';
+import 'package:project/model/Mission.dart';
+import 'package:project/model/packing_item.dart';
+import 'package:project/services/firestore_repository.dart';
+
+void main() {
+  group("Point", () {
+    PackingItem pi1;
+    setUp(() {
+      pi1 = PackingItem("stretcher", "second", 1, true);
+    });
+
+    test("Test that PackingItem.toMap() returns a map, as expected", () {
+      //pi1.toMap();
+      pi1 = PackingItem("stretcher", "second", 1, true);
+      Map<String, dynamic> pi1Map = pi1.toMap();
+      expect(pi1Map["name"], "stretcher");
+      expect(pi1Map["shelf"], "second");
+      expect(pi1Map["count"], 1);
+      expect(pi1Map["packed"], true);
+    });
+  });
+}
